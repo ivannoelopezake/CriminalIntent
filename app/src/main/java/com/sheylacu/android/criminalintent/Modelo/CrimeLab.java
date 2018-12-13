e9704891ef806c4fb82bd02f6c79dce85b422dfe
ivannoelopezake <13470371@itcampeche.edu.mx>
2018-12-13 13:52:03 -0600

    Initial commit

---
+++ app/src/main/java/com/sheylacu/android/criminalintent/Modelo/CrimeLab.java
@@ -0,0 +1,40 @@
+package com.sheylacu.android.criminalintent.Modelo;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context){
+        if (sCrimeLab == null){
+            sCrimeLab = new CrimeLab( context );
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context){
+        mCrimes = new ArrayList<>();
+        for (int i=1; i<=100; i++){
+            Crime crime = new Crime();
+            crime.setTitle( "Crimen #" + i );
+            crime.setSolved( i % 2 == 0 );
+            mCrimes.add( crime );
+        }
+    }
+    public List<Crime> getCrimes(){
+        return mCrimes;
+    }
+    public Crime getCrime(UUID id){
+        for (Crime crime : mCrimes){
+            if (crime.getId().equals(id)){
+                return crime;
+            }
+        }
+        return null;
+    }
+}
